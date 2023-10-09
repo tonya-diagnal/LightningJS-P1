85b362a9364160294ac32e77bf61454b5902ea97
tonya-Diagnal <141700782+tonya-diagnal@users.noreply.github.com>
2023-10-09 16:44:15 +0530

    Create java-js bridge only when in webview

--- src/App.js
+++ src/App.js
@@ -9,49 +9,34 @@ export default class App extends Lightning.Component {
       Container: {
         x: 0,
         y: 0,
-
         w: 1920,
         h: 1080,
         color: 0xffff0000,
-
         rect: true,
-
-        // HolePunch: {
         shader: {
           type: Lightning.shaders.Hole,
-          // w: 800,
-          // h: 550,
           w: HOLE_W,
           h: HOLE_H,
           x: 960,
           y: 0,
         },
-        // },
       },
-      // },
     };
   }
 
-  // _init() {
-  //   const canvas = this.stage.getCanvas();
-  //   canvas.style.backgroundColor = "0x00000000";
-  // }
   _init() {
     let data = {
       width: this.tag("Container").shader.w,
       height: this.tag("Container").shader.h,
     };
-    showAndroidToast(JSON.stringify(data));
-    // console.log(this.tag("Container").shader.h);
-    // showAndroidToast("hi how are you");
-    // this.tag("HolePunch")
+    var ua = navigator.userAgent.toLowerCase();
+    var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+    if (isAndroid) {
+      showAndroidToast(JSON.stringify(data));
+    }
   }
 }
 
-// const App = new BasicUsageExample({
-//   stage: { w: window.innerWidth, h: window.innerHeight, useImageWorker: false },
-// });
-// document.body.appendChild(App.stage.getCanvas());
 function showAndroidToast(toast) {
   Android.showToast(toast);
 }
