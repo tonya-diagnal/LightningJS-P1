a3ebd840732b6f200db8a0b49bea883c7632c638
gowripriya2 <56103732+gowripriya2@users.noreply.github.com>
2023-10-10 17:39:43 +0530

    Added playbutton and senddata

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Lightning } from "@lightningjs/sdk";
+import { Lightning, Utils } from "@lightningjs/sdk";
 
 let HOLE_W = 960;
 let HOLE_H = 540;
@@ -13,6 +13,18 @@ export default class App extends Lightning.Component {
         h: 1080,
         color: 0xffff0000,
         rect: true,
+        Playbutton: {
+          // rect: true,
+          src: Utils.asset("/images/play.png"),
+          zIndex: 99,
+          shader: null,
+          // color: 0xff000000,
+          w: 100,
+          h: 100,
+          y: 270,
+          x: 1440,
+          mount: 0.5,
+        },
         shader: {
           type: Lightning.shaders.Hole,
           w: HOLE_W,
@@ -35,8 +47,23 @@ export default class App extends Lightning.Component {
       showAndroidToast(JSON.stringify(data));
     }
   }
+
+  _handleEnter() {
+    let playData = { isClicked: true };
+    var ua = navigator.userAgent.toLowerCase();
+    var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+    if (isAndroid) {
+      senData(JSON.stringify(playData));
+    }
+    // console.log("key entered");
+    // this.tag("Playbutton").text.text = "Search";
+  }
 }
 
 function showAndroidToast(toast) {
   Android.showToast(toast);
+}
+
+function senData(data) {
+  Android.playMedia(data);
 }
